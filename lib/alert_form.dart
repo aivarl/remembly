46cb837d990caf8a684e5cc2c9afba51131a2cb0
Aivar Lobjakas <aivarlobjakas@gmail.com>
2020-05-14 20:51:21 +0300

    RMB-3 Get scheduled (daily) notifications working

--- lib/alert_form.dart
+++ lib/alert_form.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:CWCFlutter/bloc/alert_bloc.dart';
 import 'package:CWCFlutter/db/database_provider.dart';
 import 'package:CWCFlutter/events/add_alert.dart';
@@ -22,12 +24,14 @@ class AlertForm extends StatefulWidget {
 class AlertFormState extends State<AlertForm> {
   String _name;
   String _description;
+
   //TODO think of default values that make sense.
   TimeOfDay _startTime = TimeOfDay.now();
   TimeOfDay _endTime = TimeOfDay.now();
   bool _enabled = true;
 
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
@@ -76,12 +80,11 @@ class AlertFormState extends State<AlertForm> {
       onTap: _pickStartTime,
     );
   }
+
   _pickStartTime() async {
-    TimeOfDay t = await showTimePicker(
-        context: context,
-        initialTime: _startTime
-    );
-    if(t != null)
+    TimeOfDay t =
+        await showTimePicker(context: context, initialTime: _startTime);
+    if (t != null)
       setState(() {
         _startTime = t;
       });
@@ -94,13 +97,11 @@ class AlertFormState extends State<AlertForm> {
       onTap: _pickEndTime,
     );
   }
+
   //TODO should be possible to reuse this function, but gave an error - maybe examine later
   _pickEndTime() async {
-    TimeOfDay t = await showTimePicker(
-        context: context,
-        initialTime: _endTime
-    );
-    if(t != null)
+    TimeOfDay t = await showTimePicker(context: context, initialTime: _endTime);
+    if (t != null)
       setState(() {
         _endTime = t;
       });
@@ -168,12 +169,13 @@ class AlertFormState extends State<AlertForm> {
                         );
 
                         DatabaseProvider.db.insert(alert).then(
-                              (storedAlert) => BlocProvider.of<AlertBloc>(context).add(
+                              (storedAlert) =>
+                                  BlocProvider.of<AlertBloc>(context).add(
                                 AddAlert(storedAlert),
                               ),
                             );
                         //TODO I guess here I should schedule the notification
-                        _showNotification();
+                        _periodicallyShow();
 
                         Navigator.pop(context);
                       },
@@ -203,12 +205,14 @@ class AlertFormState extends State<AlertForm> {
                             );
 
                             DatabaseProvider.db.update(widget.alert).then(
-                                  (storedAlert) => BlocProvider.of<AlertBloc>(context).add(
+                                  (storedAlert) =>
+                                      BlocProvider.of<AlertBloc>(context).add(
                                     UpdateAlert(widget.alertIndex, alert),
                                   ),
                                 );
                             //TODO I guess here I should schedule the notification
-                            _showNotification();
+//                            _scheduleNotification();
+                            _periodicallyShow();
 
                             Navigator.pop(context);
                           },
@@ -229,15 +233,70 @@ class AlertFormState extends State<AlertForm> {
     );
   }
 
-  Future<void> _showNotification() async {
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        'your channel id', 'your channel name', 'your channel description',
-        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
-    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
-    var platformChannelSpecifics = NotificationDetails(
+  //TODO params for laters
+  Future _scheduleNotification() async {
+    var scheduledNotificationDateTime =
+    new DateTime.now().add(new Duration(seconds: 5));
+    var vibrationPattern = new Int64List(4);
+    vibrationPattern[0] = 0;
+    vibrationPattern[1] = 1000;
+    vibrationPattern[2] = 5000;
+    vibrationPattern[3] = 2000;
+
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'your other channel id',
+        'your other channel name',
+        'your other channel description',
+        icon: 'secondary_icon',
+//        sound: 'slow_spring_board',
+//        largeIcon: 'sample_large_icon',
+//        largeIconBitmapSource: BitmapSource.Drawable,
+        vibrationPattern: vibrationPattern,
+        color: const Color.fromARGB(255, 255, 0, 0));
+    var iOSPlatformChannelSpecifics =
+    new IOSNotificationDetails(sound: "slow_spring_board.aiff");
+    var platformChannelSpecifics = new NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-        0, 'plain title', 'plain body', platformChannelSpecifics,
-        payload: 'item x');
+    await flutterLocalNotificationsPlugin.schedule(
+        0,
+        'scheduled title',
+        'scheduled body',
+        scheduledNotificationDateTime,
+        platformChannelSpecifics);
   }
+//  Future<void> periodicallyShow(int id, String title, String body,
+//      RepeatInterval repeatInterval, NotificationDetails notificationDetails,
+//      {String payload}) async {
+    Future<void> _periodicallyShow() async {
+      var time = Time(20, 49, 0);
+      var androidPlatformChannelSpecifics =
+      AndroidNotificationDetails('repeatDailyAtTime channel id',
+          'repeatDailyAtTime channel name', 'repeatDailyAtTime description');
+      var iOSPlatformChannelSpecifics =
+      IOSNotificationDetails();
+      var platformChannelSpecifics = NotificationDetails(
+          androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.showDailyAtTime(
+          0,
+          'show daily title',
+          'Daily notification shown at approximately ${_toTwoDigitString(time.hour)}:${_toTwoDigitString(time.minute)}:${_toTwoDigitString(time.second)}',
+          time,
+          platformChannelSpecifics);
+    }
+
+  String _toTwoDigitString(int value) {
+    return value.toString().padLeft(2, '0');
+  }
+
+    Future<void> _showNotification() async {
+      var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+          'your channel id', 'your channel name', 'your channel description',
+          importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+      var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+      var platformChannelSpecifics = NotificationDetails(
+          androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.show(
+          0, 'plain title', 'helllo good sir', platformChannelSpecifics,
+          payload: 'item x');
+    }
 }
